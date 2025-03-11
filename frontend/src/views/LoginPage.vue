50c9a45008a6cf874fd38dbcc8c0172f3dfbcb37
Shourya <shouryagupta597@gmail.com>
2025-03-11 11:12:54 +0530

    landing-login signup page

---
+++ frontend/src/views/LoginPage.vue
@@ -0,0 +1,73 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+
+
+<template>
+<main class="h-screen w-screen flex justify-center items-center ">
+    <video autoplay muted loop class="absolute top-0 left-0 w-full h-full object-cover">
+      <source src="/a.mp4" type="video/mp4" />
+      Your browser does not support the video tag.
+    </video>
+
+    <div class="h-[80%]  w-[80%] sm:w-[40%] lg:h-[63%] p-5 rounded-3xl border border-gray-500 backdrop-blur-lg bg-opacity-30 bg-white/10">
+
+      <RouterLink to="/" class="absolute top-4 left-4 text-gray-400    hover:text-blue-400 transition flex items-center">
+        <img src="/back.svg "  class=" w-7 h-7 mr-5" alt="">
+      </RouterLink> 
+      
+        <h1 class="text-3xl font-bold text-center text-gray-300 mb-3 ">Log-In</h1>
+        <form class="space-y-4">
+        
+        <!-- Email -->
+        <div>
+          <label class="text-gray-300 text-sm font-medium">Email Address</label>
+          <input type="email" placeholder="Enter your email"
+            class="w-full mt-1 px-4 py-2 bg-gray-800 text-white rounded-lg border border-gray-700 focus:ring-2 focus:ring-blue-500 focus:outline-none">
+        </div>
+
+        <!-- Password -->
+        <div>
+          <label class="text-gray-300 text-sm font-medium">Password</label>
+          <input type="password" placeholder="Enter password"
+            class="w-full mt-1 px-4 py-2 bg-gray-800 text-white rounded-lg border border-gray-700 focus:ring-2 focus:ring-blue-500 focus:outline-none">
+        </div>
+
+        <!--roles-->
+        <div>
+          <label for="Role" class="text-gray-300 text-sm font-medium w-[20%]">Select Role</label>
+          <select id="Role" class="m-6 w-[50%] rounded-lg p-3 bg-gray-800 text-gray-400 border border-gray-700 " placeholder="Select Role">
+            <option value="techer">Teacher</option>
+            <option value="student">Student</option>
+          </select>
+        </div>
+
+        <!-- Submit Button -->
+        <button class="w-full bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 rounded-lg transition-all">
+        Log In
+        </button>
+      </form>
+ <!-- OR Divider -->
+      <div class="flex items-center justify-center my-4">
+        <div class="w-1/3 h-[1px] bg-gray-700"></div>
+        <span class="text-gray-400 text-sm mx-3">OR</span>
+        <div class="w-1/3 h-[1px] bg-gray-700"></div>
+      </div>
+
+      <!-- Google Sign-Up Button (Non-functional for now) -->
+      <button class="w-full flex items-center justify-center bg-gray-800 text-white font-semibold py-2 rounded-lg border border-gray-600 hover:bg-gray-700 transition">
+        <img src="https://www.svgrepo.com/show/355037/google.svg" class="w-5 h-5 mr-2" alt="Google Logo">
+       LogIn with Google
+      </button>
+
+      
+
+
+      <p class="text-gray-400 text-center mt-4 text-sm">
+    Does not have an account?
+        <RouterLink to="/signup" class="text-blue-400 hover:text-blue-500">Sign-up</RouterLink>
+      </p>
+    </div>
+
+</main>
+</template>
